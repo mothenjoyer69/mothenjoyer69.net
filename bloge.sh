74f45167a1a6f325a5ba90094823302a41582f13
moth <moth@mothtek.net>
2025-09-25 17:27:04 +1000

    bloge.sh: added html id insertion

--- bloge.sh
+++ bloge.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 # usage: ./bloge.sh input.md output.html
-
-html_template="$(dirname "$0")/blog/template.html"
+MD="$1"
+HTML="$2"
+TEMPLATE="$(dirname "$0")/blog/template.html"
 if [ "$#" -ne 2 ]; then
   echo "usage: $0 input.md output.html"
   exit 1
 fi
 
-MD="$1"
-HTML="$2"
 
-MD_HTML=$(pandoc --from markdown --to html "$MD")
+# convert md to html and add ids so i can format it later
+MD_HTML=$(pandoc --from markdown --to html "$MD" | sed -E 's/<h([1-6])[^>]*>/<h\1 id="mdheader\1">/g')
 
+# insert it
 awk -v content="$MD_HTML" '
   BEGIN {found=0}
   /<!-- BLOG_CONTENT -->/ {
@@ -20,4 +21,4 @@ awk -v content="$MD_HTML" '
     next
   }
   {print}
-' "$html_template" > "$HTML"
+' "$TEMPLATE" > "$HTML"
